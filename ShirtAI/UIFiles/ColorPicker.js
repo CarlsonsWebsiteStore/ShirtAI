b2ea24a0abfc9f39e93e329332fa16771640b9f1
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-05-01 00:04:55 -0400

    Basic UI For shirt site AI

---
+++ ShirtAI/UIFiles/ColorPicker.js
@@ -0,0 +1,28 @@
+let eventButton = document.getElementById("colorButton");
+let redShadeCodes = {
+    red: "#ff6666",
+    maroon: "#800000",
+    tomato: "#FF6347",
+    salmon: "#FA8072",
+    lightsalmon: "#FFA07A",
+    firebrick: "#B22222",
+    darkred: "#8B0000",
+    orangered: "#FF4500",
+    palevioletred: "#DB7093",
+    darksalmon: "#E9967A"
+};
+
+let blueShadeCodes = {
+    blue: "#0000FF",
+    aqua: "#6edbff",
+    lightcyan: "#E0FFFF",
+    cyan: "#00FFFF",
+    lightblue: "#ADD8E6",
+    bluejay: "#2B547E",
+    marbleblue: "#566D7E",
+    steelblue: "#4863A0",
+    earthblue: "#0000A0",
+    navyblue: "#000080",
+    blueberryblue: "#0041C2",
+
+};
